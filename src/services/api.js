587326b05b076af5ecc2f536eb626307586bea63
DIPanjan198 <dipchoudhuri23@gmail.com>
2026-02-14 23:00:04 +0530

    APi

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://civic-connect-api.onrender.com/api"
+      ? "https://civic-backend-00ge.onrender.com/api"
       : "http://localhost:5000/api",
 });
 
